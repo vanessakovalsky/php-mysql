877ef43ba48b80f96bf53fc41a3f5e3561f3fe67
Vanessa DAVID <vanessa.david@alten.com>
2018-03-06 17:11:08 +0100

    Ajout correction tp3

---
+++ docs/tp3-structure-controle/corrige/connexion.php
@@ -0,0 +1,59 @@
+<?php include_once('includes/header.php');
+
+if($is_connected == FALSE){
+?>
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Jumbotron Header -->
+      <header class="jumbotron my-4">
+        <h1 class="display-3"><?php echo $nom_connexion;?></h1>
+        <p class="lead"><?php echo $texte_connexion;?></p>
+        <a href="#" class="btn btn-primary btn-lg"><?php echo $inscription;?></a>
+      </header>
+
+      <!-- Page Features -->
+      <div class="row text-center">
+
+        <form class="form-signin">
+          <h1 class="h3 mb-3 font-weight-normal"><?php echo $merci_connecter;?></h1>
+          <label for="inputEmail" class="sr-only"><?php echo $login;?></label>
+          <input type="email" id="inputEmail" class="form-control" placeholder="<?php echo $login;?>" required autofocus>
+          <label for="inputPassword" class="sr-only"><?php echo $password;?></label>
+          <input type="password" id="inputPassword" class="form-control" placeholder="<?php echo $password;?>" required>
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"><?php echo $remember;?>
+            </label>
+          </div>
+          <button class="btn btn-lg btn-primary btn-block" type="submit"><?php echo $nom_connexion;?></button>
+        </form>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+<?php
+}
+else { ?>
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+      <h1 class="display-3">Bienvenue sur votre profil</h1>
+      <p class="lead"><?php echo $texte_connexion;?></p>
+      <a href="#" class="btn btn-primary btn-lg"><?php echo $inscription;?></a>
+    </header>
+
+    <!-- Page Features -->
+    <div class="row text-center">
+      <p>Vous êtes déjà connecté!</p>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+<?php
+}
+include_once('includes/footer.php'); ?>
