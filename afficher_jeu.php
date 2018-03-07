18e57b24a6f0905b78b526e3f8fdd8a0312a5f47
Vanessa DAVID <vanessa.david@alten.com>
2018-03-07 11:31:25 +0100

    ajout traitement formulaire + affichage du jeu

--- afficher_jeu.php
+++ afficher_jeu.php
@@ -1,12 +1,33 @@
 <?php
-print_r($_POST);
 
+include_once('includes/header.php');
+
+//On traite les données du formulaire reçues dans $_POST
 $nouveau_jeu = $_POST['nom_jeu'].';'.$_POST['editeur'].';'.$_POST['annee'].';'.$_POST['descriptif'].';'.$_POST['categorie'].';'.$_POST['duree'].';'.$_POST['nb_joueur'].';'.$_POST['commentaire'];
+/* On ouvre le fichier en écriture uniquement (a), pour aller directement à la
+*  fin du fichier
+*/
 $file = fopen('jeux.txt','a');
-$br = fputs($file,"\n");
-$ecrire = fputs($file,$nouveau_jeu);
-echo $ecrire;
+$ecrire = fputs($file,$nouveau_jeu."\n");
 fclose($file);
 echo 'votre nouveau jeu a été ajouté';
 
  ?>
+
+     <!-- Page Content -->
+     <div class="container">
+
+       <!-- Jumbotron Header -->
+       <header class="jumbotron my-4">
+         <h1 class="display-3"><?php echo $_POST['nom_jeu'];?></h1>
+         <p class="lead"><?php echo $_POST['descriptif'];?></p>
+         <a href="#" class="btn btn-primary btn-lg"><?php echo $inscription; ?></a>
+       </header>
+
+       <!-- Page Features -->
+
+
+     </div>
+     <!-- /.container -->
+
+ <?php include_once('includes/footer.php'); ?>
